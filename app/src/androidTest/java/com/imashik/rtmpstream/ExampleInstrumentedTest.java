1dad28d8235a0a3e052290cb061b0f6586348f39
Mohamed Ashik <ashik@xcelcorp.com>
2021-12-09 16:25:58 +0530

    minor corrections

--- app/src/androidTest/java/com/imashik/rtmpstream/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/imashik/rtmpstream/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.imashik.usbcamtest", appContext.getPackageName());
+        assertEquals("com.imashik.rtmpstream", appContext.getPackageName());
     }
 }
